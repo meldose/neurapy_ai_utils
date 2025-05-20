11a7273c5caf8240d7780b1b90b3ef2a6f496e75
Midhun Eldose <midhun.eldose@neura-robotics.com>
2025-05-20 14:52:35 +0200

    test_action_pick.py

--- test_action_hand_detection.py
+++ test_action_hand_detection.py
@@ -1,4 +1,4 @@
-import rospy
+import rospy # imported rospy module
 from neurapy_ai.clients.edge_refiner_client import EdgeRefinerClient
 from neurapy_ai.clients.hand_detection_client import HandDetectionClient
 from neurapy_ai.utils.return_codes import ReturnCodes
@@ -9,33 +9,35 @@ from neurapy_ai_utils.robot.robot import Robot
 
 # calling the main function
 if __name__ == "__main__":
-    hand_detection_client = HandDetectionClient()
-    rospy.init_node("application_run")
+    hand_detection_client = HandDetectionClient() # setting up the hand detection client 
+    rospy.init_node("application_run") # initilase the node 
     robot = Robot(MoveitKinematics(), DummyGripper())
-    # observe_position = [0, 1, 1, 0, 0, 0, 1]
-    # drop_position = [0, 0.1, 1, 0, 0, 0, 1]
 
-    observe_position = [0, 0, 0, 1.5, -1.5, 0, 0]
+    observe_position = [0, 0, 0, 1.5, -1.5, 0, 0] # setting up the observe position
 
-    robot.move_joint_to_joint(observe_position)
+    robot.move_joint_to_joint(observe_position) # moving robot to the observe position
 
     return_code, waypoits = hand_detection_client.detect_hand_poses(2)
+    
+    # if the return code is not success
     if return_code != ReturnCodes.SUCCESS or len(waypoits) == 0:
         print("Something went bad")
         exit(0)
 
     print(waypoits)
 
-    edge_detection_client = EdgeRefinerClient()
+    edge_detection_client = EdgeRefinerClient() # setting up the EdgeRefinerClient 
     [
         return_code,
         point_begining,
         point_end,
     ] = edge_detection_client.refine_line(waypoits[0], waypoits[1], False)
+
+# if the return code is not success
     if return_code != ReturnCodes.SUCCESS or len(waypoits) == 0:
-        print("Something went bad")
+        print("Something went bad") # not success
         exit(0)
-    print(point_begining)
+    print(point_begining) 
     print(point_end)
-    robot.move_joint_to_cartesian(point_begining)
-    robot.move_linear(point_end)
+    robot.move_joint_to_cartesian(point_begining)  # robot moving to the point begining 
+    robot.move_linear(point_end) # robot moving to the point end 
