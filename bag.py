744ccebe476776955db48ada1ee0db50480ac4a8
Midhun Eldose <midhun.eldose@neura-robotics.com>
2025-06-24 22:28:34 +0200

    bag.py

--- bag.py
+++ bag.py
@@ -1,26 +1,26 @@
 from rosbags.highlevel import AnyReader # imported rosbags modules
 from pathlib import Path # imported the path
 import cv2 # imported the cv2 module 
-from rosbags.image import message_to_cvimage
+from rosbags.image import message_to_cvimage # importing the image 
 
 
 # function for getting the images
 def get_images(bagdir, outdir):
-    outdir = Path(outdir)
-    if not outdir.exists():
+    outdir = Path(outdir) #setting up the ouput dir
+    if not outdir.exists(): # if not existing 
         outdir.mkdir()
     count = 0
     with AnyReader([Path(bagdir)]) as reader:
         # topic and msgtype information is available on .connections list
         for connection in reader.connections:
-            print(connection.topic, connection.msgtype)
+            print(connection.topic, connection.msgtype) #print he connection topic and msgtype
         for connection, timestamp, rawdata in reader.messages():
             if (
-                connection.topic == "/camera/camera/color/image_raw"
+                connection.topic == "/camera/camera/color/image_raw" # check if the topic exist or not 
             ):  # topic Name of images
                 msg = reader.deserialize(rawdata, connection.msgtype)
                 # change encoding type if needed
-                img = message_to_cvimage(msg, "bgr8")
+                img = message_to_cvimage(msg, "bgr8") # function to convert the messaget to cv image 
                 cv2.imwrite(outdir / f"img_{count}.png", img)
                 count += 1
 
